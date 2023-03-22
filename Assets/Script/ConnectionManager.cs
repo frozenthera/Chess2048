018fdad4df86c155cd28e3255bf2c940c61d4c93
FrozenThera <dongmin11004@korea.ac.kr>
2023-03-23 03:31:30 +0900

    Implementing lobby scene

--- Assets/Script/ConnectionManager.cs
+++ Assets/Script/ConnectionManager.cs
@@ -6,11 +6,33 @@ using Unity.Services.Core;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ConnectionManager : MonoBehaviour
 {
     static string code = "";
     static bool isPressed = false;
+    static bool isLoaded = false;
+    NetworkManager NetworkManager;
+
+    protected void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        NetworkManager = NetworkManager.Singleton;
+        InitUnityService();
+        NetworkManager.OnClientConnectedCallback += OnClientConnectedCallback;
+        NetworkManager.OnClientDisconnectCallback += OnClientDisconnectedCallback;
+    }
+
+    private void OnDestroy()
+    {
+        NetworkManager.OnClientConnectedCallback -= OnClientConnectedCallback;
+        NetworkManager.OnClientDisconnectCallback -= OnClientDisconnectedCallback;
+    }
 
     void OnGUI()
     {
@@ -31,11 +53,6 @@ public class ConnectionManager : MonoBehaviour
 
     static void StartButtons()
     {
-        /*
-        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();*/
-
         if (GUILayout.Button("Host"))
         {
             StartHosting();
@@ -47,11 +64,8 @@ public class ConnectionManager : MonoBehaviour
         }
     }
 
-    static async void StartHosting()
+    async void InitUnityService()
     {
-        if (isPressed) return;
-        isPressed = true;
-
         //Initialize the Unity Services engine
         await UnityServices.InitializeAsync();
         //Always autheticate your users beforehand
@@ -60,6 +74,15 @@ public class ConnectionManager : MonoBehaviour
             //If not already logged, log the user in
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
         }
+
+        isLoaded = true;
+    }
+
+
+    static async void StartHosting()
+    {
+        if (isPressed) return;
+        isPressed = true;
 
         Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
         string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
@@ -75,15 +98,6 @@ public class ConnectionManager : MonoBehaviour
     {
         if (isPressed) return;
         isPressed = true;
-
-        //Initialize the Unity Services engine
-        await UnityServices.InitializeAsync();
-        //Always authenticate your users beforehand
-        if (!AuthenticationService.Instance.IsSignedIn)
-        {
-            //If not already logged, log the user in
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
 
         //Ask Unity Services to join a Relay allocation based on our join code
         JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(code);
@@ -103,4 +117,29 @@ public class ConnectionManager : MonoBehaviour
         GUILayout.Label("Mode: " + mode);
         GUILayout.Label("JoinCode: " + code);
     }
+
+    private void OnClientConnectedCallback(ulong clientId)
+    {
+        if (NetworkManager.IsHost)
+        {
+            NetworkManager.SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+        }
+        Debug.Log($"{clientId} is connected");
+    }
+
+    private void OnClientDisconnectedCallback(ulong clientId)
+    {
+        if (NetworkManager.IsHost)
+        {
+            NetworkManager.SceneManager.LoadScene("LobbyScene", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene("LobbyScene");
+        }
+
+        isPressed = false;
+        Debug.Log($"{clientId} is disconnected");
+    }
+
 }
